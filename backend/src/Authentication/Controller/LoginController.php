dc069f716c4420a2389d3dfcd640e130e5fda9bf
DiosDuck <tapucovidiu@gmail.com>
2025-11-29 15:13:34 +0200

    create login endpoint

--- backend/src/Authentication/Controller/LoginController.php
+++ backend/src/Authentication/Controller/LoginController.php
@@ -9,6 +9,10 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use OpenApi\Attributes as OA;
+use App\Authentication\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 #[Route(path: '/api/auth', name: 'auth_')]
 class LoginController extends AbstractController {
@@ -19,12 +23,61 @@ class LoginController extends AbstractController {
         summary: 'Login user',
         tags: ['Authentication'],
     )]
+    #[OA\RequestBody(
+        description: 'Request Body',
+        required: true,
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    property: 'username',
+                    type: 'string',
+                    example: 'username'
+                ),
+                new OA\Property(
+                    property: 'password',
+                    type: 'string',
+                    example: 'password',
+                )
+            ]
+        )
+    )]
     #[OA\Response(
         response: 200,
-        description: 'OK',
+        description: 'User is logged in',
     )]
-    public function login(Request $request): JsonResponse
+    #[OA\Response(
+        response: 401,
+        description: 'Bad credentials',
+    )]
+    public function login(#[CurrentUser] ?User $user): JsonResponse
     {
+        if (null === $user) {
+            return $this->json(
+                ['message' => 'Wrong credentials'],
+                JsonResponse::HTTP_UNAUTHORIZED,
+            );
+        }
+        
+        return $this->json([
+            'message' => 'ok',
+            'displayName' => $user->getDisplayName(),
+        ]);
+    }
+
+    public function devRegister(UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $user = new User();
+        
+        $user->setDisplayName('Test User');
+        $user->setEmail('test@email.com');
+        $user->setUsername('username');
+
+        $hashedPassword = $passwordHasher->hashPassword($user, 'password');
+        $user->setPassword($hashedPassword);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
         return $this->json(['message' => 'ok']);
     }
 }

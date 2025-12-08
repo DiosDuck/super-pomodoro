01d75b719c00f18474fbc463bd2e1cdcb1180491
DiosDuck <tapucovidiu@gmail.com>
2025-12-08 11:29:10 +0200

    first step for registration

--- backend/src/Authentication/Controller/AuthenticationController.php
+++ backend/src/Authentication/Controller/AuthenticationController.php
@@ -4,13 +4,66 @@ declare(strict_types=1);
 
 namespace App\Authentication\Controller;
 
+use App\Authentication\DTO\RegisterUserDTO;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Authentication\Entity\User;
+use App\Authentication\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Nelmio\ApiDocBundle\Attribute\Model;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use OpenApi\Attributes as OA;
 
+#[Route(path: '/api/auth', name: 'api_auth_')]
 class AuthenticationController extends AbstractController {
+    
+    #[Route(path: '/register', name: 'register', methods: ['PUT'])]
+    #[OA\Put(
+        path: '/api/auth/register',
+        summary: 'Register user',
+        tags: ['Authentication'],
+    )]
+    #[OA\RequestBody(
+        content: new OA\JsonContent(
+            ref: new Model(type: RegisterUserDTO::class),
+        )
+    )]
+    #[OA\Response(
+        response: 200,
+        description: 'User has been registered',
+    )]
+    #[OA\Response(
+        response: 400,
+        description: 'Invalid user data',
+    )]
+    public function register(
+        #[MapRequestPayload] RegisterUserDTO $registerUser,
+        UserRepository $userRepository,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $entityManager
+    ): JsonResponse {
+        if ($userRepository->findOneBy(['username' => $registerUser->username])) {
+            return $this->json(['message' => 'Invalid user data'], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        $user = new User();
+        
+        $user->setDisplayName($registerUser->displayName);
+        $user->setEmail($registerUser->email);
+        $user->setUsername($registerUser->username);
+        $user->setRoles(['ROLE_USER']);
+
+        $hashedPassword = $passwordHasher->hashPassword($user, $registerUser->password);
+        $user->setPassword($hashedPassword);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->json(['message' => 'ok']);
+    }
+
     public function devRegister(UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): JsonResponse
     {
         $user = new User();
---
+++ backend/src/Authentication/DTO/RegisterUserDTO.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Authentication\DTO;
+
+use OpenApi\Attributes as OA;
+
+#[OA\Schema(
+    schema: 'RegisterUserDTO',
+    title: 'Register User Schema',
+    description: 'Register User Schema for register into DB',
+)]
+class RegisterUserDTO {
+    #[OA\Property(example: 'john_smith')]
+    public string $username;
+    #[OA\Property(example: 'password')]
+    public string $password;
+    #[OA\Property(example: 'john.smith@email.com')]
+    public string $email;
+    #[OA\Property(example: 'John Smith')]
+    public string $displayName;
+}

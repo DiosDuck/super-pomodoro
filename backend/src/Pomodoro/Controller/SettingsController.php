79f0fee96367ab65f64c0915a8cac17a694ea21c
DiosDuck <tapucovidiu@gmail.com>
2025-12-28 23:03:09 +0200

    create settings in doctrine

---
+++ backend/src/Pomodoro/Controller/SettingsController.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Pomodoro\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SettingsController extends AbstractController {
+
+}
--- frontend/src/app/pomodoro/components/index/index.ts
+++ frontend/src/app/pomodoro/components/index/index.ts
@@ -1,6 +1,7 @@
 import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { CounterService } from '../../pomodoro.services';
 import { RouterLink } from "@angular/router";
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pomodoro-index',
@@ -10,8 +11,9 @@ import { RouterLink } from "@angular/router";
 })
 export class Index implements OnInit {
   counterService = inject(CounterService);
+  title = inject(Title);
   cycle = this.counterService.cycle;
-  title = computed(
+  header = computed(
     () => {
       if (this.isWaitingFormConfirmation()) {
         return 'Continue?'
@@ -37,10 +39,34 @@ export class Index implements OnInit {
   isWaitingFormConfirmation = this.counterService.waitingConfirmation;
   alarmClockAudio = new Audio('assets/audio/alarm-clock.mp3');
 
+  titleValue = computed(
+    () => {
+      let start: string;
+      if (this.isWaitingFormConfirmation()) {
+        start = 'Confirm';
+      } else {
+        switch (this.cycle().currentCycle) {
+          case 'idle':
+            return 'Pomodoro';
+          case 'work':
+            start = 'Work';
+            break;
+          case 'short-break':
+          case 'long-break':
+            start = 'Break';
+            break;
+        }
+      }
+
+      return `${start} ${this.minutes()}:${this.seconds()}`;
+    }
+  );
+
   ngOnInit(): void 
   {
     this.counterService.remainingSeconds.subscribe((time: number) => {
       this.timer.set(time);
+      this.title.setTitle(this.titleValue());
     });
     this.counterService.finish.subscribe(() => {
       if (this.isWaitingFormConfirmation()) {

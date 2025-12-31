fe986c465f6951e5d60cdbc7732c934ecde7d5a6
DiosDuck <tapucovidiu@gmail.com>
2025-12-31 16:35:22 +0200

    add animation on toasts

--- frontend/src/app/shared/components/toast/line/line.ts
+++ frontend/src/app/shared/components/toast/line/line.ts
@@ -1,17 +1,40 @@
-import { Component, input, output } from "@angular/core";
+import { Component, input, OnInit, output, signal } from "@angular/core";
 import { Toast } from "../../../models/toast";
+import { BehaviorSubject, delay, interval, Subject, takeUntil, takeWhile, timer } from "rxjs";
 
 @Component({
   selector: 'app-toast-line',
   templateUrl: './line.html',
   styleUrl: './line.scss',
 })
-export class Line {
+export class Line implements OnInit{
+    isClosed = signal<boolean>(false);
+    private _finish = new Subject<void>();
+    private _observableFinish = this._finish.asObservable();
+
     toast = input.required<Toast>();
+    timeUp = input<number>(5);
     close = output<number>();
+
+    ngOnInit(): void {
+      timer(this.timeUp() * 1000)
+        .pipe(
+          takeUntil(this._observableFinish)
+        )
+        .subscribe(() => {
+          this._finish.next();
+        })
+      ;
+
+      this._observableFinish.subscribe(() => {
+        this.isClosed.set(true);
+        timer(1000)
+          .subscribe(() => this.close.emit(this.toast().id))
+      })
+    }
 
     onClose(): void
     {
-        this.close.emit(this.toast().id);
+      this._finish.next();
     }
 }
